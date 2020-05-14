9bd719ad277a29e7d5953fc27d803535c107e2bd
ArtakKostanyan <artak101991@gmail.com>
2020-05-14 22:55:35 +0400

    Update User.js

--- resources/js/components/User.js
+++ resources/js/components/User.js
@@ -1,41 +1,47 @@
-import React  from 'react';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-export default function User({user}){
 
-const [color,setColor]=useState('#ffffff');
-const handleClick=(event)=>{
-    
-    console.log(event)
-    dispatch({type:'userId',payload:user.id})
-}
-   const dispatch =useDispatch();
-     return(
+export default function User({user}) {
 
+    const [color, setColor] = useState('#ffffff');
+    const handleClick = (event) => {
 
-        <div className="media" style={{
+        setColor('#a7a7a7')
+        dispatch({type: 'userId', payload: user.id})
+    }
+    const dispatch = useDispatch();
+    return (
+        <>
+            {
+                (user.id !== window.user.id) ?
+                    <div className="media" style={{
+                        background: color,
+                        marginBottom: '5px',
+                        position: 'relative',
+                        cursor: 'pointer'
+                    }}>
+                        <div className="media-left">
+                            <img src={user.avatar} alt="" className="media-object"/>
+                        </div>
 
-            marginBottom:'5px',
-            position:'relative',
-        }} >
-            <div className="media-left">
-                <img src={user.avatar } alt="" className="media-object" />
-            </div>
+                        <div className="media-body">
+                            <p className="name">{user.name}</p>
+                            <p className="email">{user.email}</p>
+                        </div>
 
-            <div className="media-body">
-                <p className="name">{user.name}</p>
-                <p className="email">{user.email}</p>
-            </div>
+                        <div style={{
 
-            <div style={{
-                background:'#a7a7a7',
-                position:'absolute',
-                width:'100%',
-                height:'100%'
+                            position: 'absolute',
+                            width: '100%',
+                            height: '100%'
 
-            }}
-                 className="overlay" onClick={handleClick}> </div>
-        </div>
+                        }}
+                             className="overlay" onClick={handleClick}></div>
+                    </div>
 
-
-    );
+                    : ''
+            }
+        </>
+    )
+        ;
 }
